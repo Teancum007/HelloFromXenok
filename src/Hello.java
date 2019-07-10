d018ec4276d5e350163294d87ec74b81448c85a8
Teancum007 <icblamires@gmail.com>
2019-07-10 10:33:24 -0600

    Added title to name.

--- src/Hello.java
+++ src/Hello.java
@@ -4,8 +4,8 @@ public class Hello {
 
         Hello me = new Hello();
 
-        System.out.println("Hello, my name is Xenok!");
+        System.out.println("Hello, my name is Sunmaster Xenok!");
 
-        //Testing... 1 2 3 ...
+        //Testing... 1 2 3...
     }
 }
